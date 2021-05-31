501c16367467657cef762f4c5d4a883217cc5a94
Jose Valencia <sejoma989@gmail.com>
2021-05-31 11:17:44 -0500

    fix: ruta redireccion despues de animacion

--- p2/javascript/main.js
+++ p2/javascript/main.js
@@ -31,7 +31,7 @@ links.forEach(function(link) {
         content.classList.add("animate__animated");
 
         setTimeout(function() {
-            location.href = "/";
+            location.href = "/pages/p2";
         }, 600);
 
         return false;
