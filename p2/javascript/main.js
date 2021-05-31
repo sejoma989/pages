0da218b60a519507e5da0ddf6f70f9b33de6295a
Jose Valencia <sejoma989@gmail.com>
2021-05-31 08:59:40 -0500

    desbloqueo button link

--- p2/javascript/main.js
+++ p2/javascript/main.js
@@ -1,13 +1,10 @@
-console.log("...cargando");
-console.log("El componente javascript ha cargado con éxito");
-
+//console.log("...cargando");
+//console.log("El componente javascript ha cargado con éxito");
 let links = document.querySelectorAll(".close");
 
 links.forEach(function(link) {
 
     link.addEventListener("click", function(event) {
-        console.log("Paila papi");
-        event.preventDefault();
-        return false;
+        // event.preventDefault();
     });
 });
